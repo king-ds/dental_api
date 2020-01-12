389e7e676a8163881a01b2739fde2bcca8b271ac
king-ds <ricardo.calura29@gmail.com>
2020-01-12 19:10:42 +0800

    Edit models.py authentication

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -27,35 +27,32 @@ class ClinicalInstructorSerializer(serializers.ModelSerializer):
 class PatientSerializer(serializers.ModelSerializer):
     username = serializers.CharField(required=True)
     first_name = serializers.CharField(required=True)
-    middle_name = serializers.CharField(required=True)
+    middle_initial = serializers.CharField(required=False, allow_blank=True, allow_null=True)
     last_name = serializers.CharField(required=True)
     password = serializers.CharField(required=True)
     title = serializers.CharField(required=True)
     gender = serializers.CharField(required=True)
     birthdate = serializers.DateField(required=True)
-    religion = serializers.CharField(allow_blank=True)
+    religion = serializers.CharField(allow_blank=True, allow_null=True)
     marital_status = serializers.CharField(required=True)
     occupation = serializers.CharField(required=True)
-    number_of_kids = serializers.IntegerField(required=True)
-    height = serializers.CharField(required=True)
-    weight = serializers.FloatField(required=True)
+    height = serializers.CharField(required=False, allow_null=True)
+    weight = serializers.FloatField(required=False, allow_null=True)
     permanent_address = serializers.CharField(required=True)
-    telephone_num = serializers.CharField(allow_blank=True)
-    phone_num = serializers.CharField(allow_blank=True)
-    email_address = serializers.EmailField(allow_blank=True)
-    emergency_first_name = serializers.CharField(required=True)
-    emergency_middle_name = serializers.CharField(required=True)
-    emergency_last_name = serializers.CharField(required=True)
+    telephone_num = serializers.CharField(allow_blank=True, allow_null=True)
+    phone_num = serializers.CharField(allow_blank=True, allow_null=True)
+    email_address = serializers.EmailField(allow_blank=True, allow_null=True)
+    emergency_full_name = serializers.CharField(required=False, allow_blank=True)
     relation_to_patient = serializers.CharField(required=True)
-    emergency_phone_num = serializers.CharField(allow_blank=True)
-    emergency_address = serializers.CharField(required=True)
-    emergency_telephone_num = serializers.CharField(allow_blank=True)
+    emergency_phone_num = serializers.CharField(allow_blank=True, allow_null=True)
+    emergency_address = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    emergency_telephone_num = serializers.CharField(allow_blank=True, allow_null=True)
 
     class Meta:
         model = Patient
-        fields = ('id', 'username', 'first_name', 'middle_name', 'last_name',
+        fields = ('id', 'username', 'first_name', 'middle_initial', 'last_name',
         'password', 'title', 'gender', 'birthdate', 'religion', 'marital_status',
-        'occupation', 'number_of_kids', 'height', 'weight', 'permanent_address',
-        'telephone_num', 'phone_num', 'email_address', 'emergency_first_name',
-        'emergency_middle_name', 'emergency_last_name', 'relation_to_patient', 
-        'emergency_phone_num', 'emergency_address', 'emergency_telephone_num', 'datetime_joined')
+        'occupation', 'height', 'weight', 'permanent_address',
+        'telephone_num', 'phone_num', 'email_address', 'emergency_full_name',
+        'relation_to_patient', 'emergency_phone_num', 'emergency_address', 
+        'emergency_telephone_num', 'datetime_joined')
